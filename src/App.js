425eb3ae7a3ac04cb2ac0d4c2f2db4b80baaf2b9
komal <komal.bhalerao@myntra.com>
2023-03-01 22:17:40 +0530

    working note without click actions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Note from './containers/Note';
 
 function App() {
+  const NotesObj = {
+    Notes: [{ headding: "headding", content: "contents is " }]
+  };
+  // saveNote = (e) => {
+  //   NotesObj.Notes[0].content = e;
+  //   const NotesString = JSON.stringify(NotesObj);
+  //   localStorage.setItem("notes", NotesString);
+  //   console.log("inside saveNote");
+  // };
   return (
     <div className="App">
+      <Note headding= {NotesObj.Notes[0].headding} content={NotesObj.Notes[0].content} />
     </div>
   );
 }
---
+++ src/containers/Note.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+export default function Note(Props) {
+  const [headding, setHeadding] = useState(Props.headding);
+  const [content, setContent] = useState(Props.content);
+  const [editable, setEditable] = useState(false);
+//   onSaveClick = () => {
+//     console.log("onSaveClick button ");
+//     setEditable(false);
+//     Props.saveNote();
+//   };
+//   const onEditClick = (e) => {
+//    // setEditable(true);
+//     console.log("onEditClick ", e);
+//   };
+//   onInputChange = (e) => {
+//     setContent(e.target.value);
+//     console.log("onInputChange", e.target.value);
+//   };
+  return (
+    <div /*className="NoteBox" */>
+      <h1> {headding} </h1>
+      {editable === true ? (
+        <>
+          <h3>
+            <input
+              type="text"
+              defaultValue={Props.content}
+             /* onChange={this.onInputChange}*/
+            />
+          </h3>
+        </>
+      ) : (
+        <>
+          <h3> {content} </h3>
+        </>
+      )}
+
+      <button className="Buttons" /*onClick={this.onEditClick}*/>
+        edit
+      </button>
+      <button className="Buttons" /*onClick={this.onSaveClick}*/>
+        save
+      </button>
+    </div>
+  );
+}
+
+/*
+Notes should do is edit content, edit heading 
+
+state 
+*/
